5afd1ef5e8a61605a07223d2c011916fc7a19f38
Tony HUerto <tonylex12@gmail.com>
2024-12-22 18:33:25 -0500

    ADD: validate existing users

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -3,7 +3,13 @@ import { Request, Response } from "express";
 import User from "../models/User";
 
 export const createAccount = async (req: Request, res: Response) => {
+  const { email } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(409).json({ message: "User already exists" });
+    return;
+  }
   const user = new User(req.body);
   await user.save();
   res.status(201).json({ message: "User created" });
-}
+};
