b5db6bbf1c0c4ef6f41caeecafcd9bdd241ffbb6
Tony HUerto <tonylex12@gmail.com>
2024-12-22 13:07:20 -0500

    ADD: IUser

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import server from "./server";
 const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
-  // console.log(`Server is running on http://localhost:${port}`);
   console.log(
     colors.blue.bold(`Server is running on http://localhost:${port}`)
   );
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,11 @@
 import moongose, { Schema } from "mongoose";
 
+export interface IUser {
+  name: string;
+  email: string;
+  password: string;
+}
+
 const userSchema = new Schema({
   name: {
     type: String,
@@ -17,6 +23,6 @@ const userSchema = new Schema({
   },
 });
 
-const User = moongose.model("User", userSchema);
+const User = moongose.model<IUser>("User", userSchema);
 
 export default User;
