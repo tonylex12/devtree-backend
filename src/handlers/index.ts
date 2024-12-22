bc9030d3d486ba537666f6297a01d02139037721
Tony HUerto <tonylex12@gmail.com>
2024-12-22 18:49:22 -0500

    ADD: bcrypt to hash passwords

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,6 +1,8 @@
-import { Request, Response } from "express";
+import bcrypt from "bcrypt";
 
+import { Request, Response } from "express";
 import User from "../models/User";
+import { hashPassword } from "../utils/auth";
 
 export const createAccount = async (req: Request, res: Response) => {
   const { email } = req.body;
@@ -10,6 +12,7 @@ export const createAccount = async (req: Request, res: Response) => {
     return;
   }
   const user = new User(req.body);
+  user.password = await hashPassword(user.password);
   await user.save();
   res.status(201).json({ message: "User created" });
 };
