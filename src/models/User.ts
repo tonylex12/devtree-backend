deca569dfdb0e1d884ad7edcf272eceb276fee44
Tony HUerto <tonylex12@gmail.com>
2024-12-22 12:32:34 -0500

    ADD: User Schema

---
+++ src/models/User.ts
@@ -0,0 +1,22 @@
+import moongose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+const User = moongose.model("User", userSchema);
+
+export default User;
