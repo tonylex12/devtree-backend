86f5be400709601066208d688b78e215e304f8b5
Tony HUerto <tonylex12@gmail.com>
2024-12-22 22:02:17 -0500

    ADD: express validator for validations

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,10 +1,16 @@
 import slug from "slug";
-
+import { validationResult } from "express-validator";
 import { Request, Response } from "express";
 import User from "../models/User";
 import { hashPassword } from "../utils/auth";
 
 export const createAccount = async (req: Request, res: Response) => {
+  // Validar campos
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() });
+    return;
+  }
   const { email } = req.body;
   const userExists = await User.findOne({ email });
   if (userExists) {
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,17 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { createAccount } from "./handlers";
 
 const router = Router();
 
 // Authentication and Register
-router.post("/auth/register", createAccount);
+router.post(
+  "/auth/register",
+  body("handle").notEmpty().withMessage("Handle is required"),
+  body("name").isLength({ min: 3 }).withMessage("Name must be at least 3 characters"),
+  body("email").isEmail().withMessage("Email must be valid"),
+  body("password").isLength({ min: 8 }).withMessage("Password must be at least 8 characters"),
+  createAccount
+);
 
 export default router;
