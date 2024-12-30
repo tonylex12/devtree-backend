b7603549269bb40e6b2cd65d50dbd77e8483112d
Tony HUerto <tonylex12@gmail.com>
2024-12-29 19:32:15 -0500

    ADD: image attribute to the User model

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -97,7 +97,9 @@ export const uploadImage = async (req: Request, res: Response) => {
             return;
           }
           if (result) {
-            console.log(result.secure_url);
+            req.user.image = result.secure_url;
+            await req.user.save();
+            res.json({ image: result.secure_url });
           }
         }
       );
--- src/models/User.ts
+++ src/models/User.ts
@@ -6,6 +6,7 @@ export interface IUser extends Document {
   email: string;
   password: string;
   description: string;
+  image: string;
 }
 
 const userSchema = new Schema({
@@ -31,6 +32,10 @@ const userSchema = new Schema({
     trim: true,
   },
   description: {
+    type: String,
+    default: "",
+  },
+  image: {
     type: String,
     default: "",
   },
